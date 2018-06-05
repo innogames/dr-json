366b76b426db18a1b9e98ba5e3f37433b7c5f927
Christian Blos <christian.blos@innogames.com>
2018-06-05 09:17:30 +0200

    zip files on yarn run dist

---
+++ build/scripts/zip-files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function getPackageJsonVal {
+    echo $(cat package.json \
+      | grep $1 \
+      | head -1 \
+      | awk -F: '{ print $2 }' \
+      | sed 's/ *"//' \
+      | sed 's/", *//')
+}
+
+DIR="$(dirname "$0")"
+PACKAGE_VERSION=$(getPackageJsonVal version)
+PRODUCT_NAME=$(getPackageJsonVal productName)
+
+echo "Zip files..."
+mkdir dist/upload
+zip -j dist/upload/Dr.Json-${PACKAGE_VERSION}-mac.zip "dist/Dr. Json-${PACKAGE_VERSION}.dmg"
+zip -j dist/upload/Dr.Json-${PACKAGE_VERSION}-windows.zip "dist/Dr. Json ${PACKAGE_VERSION}.exe"
