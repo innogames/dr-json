353a79b23f304d2ff33f56e303da8853155fdb24
Bojan Petkovski <bojan.petkovski@innogames.com>
2021-12-08 17:38:41 +0100

    Replace settings storage library

--- src/app/shared/version.ts
+++ src/app/shared/version.ts
@@ -1,13 +1,9 @@
-import {app, App} from 'electron';
-import {app as remoteApp} from '@electron/remote';
 import {isDev} from './environment';
 import {packageJson} from './package';
 
-const electronApp: App = app || remoteApp;
-
 export function getAppVersion(): string {
     if (!isDev()) {
-        return electronApp.getVersion();
+        return process.env.npm_package_version || packageJson.version;
     }
 
     return packageJson.version;
--- src/domain/context/settings/SettingsStorageInterface.ts
+++ src/domain/context/settings/SettingsStorageInterface.ts
@@ -1,7 +1,7 @@
 export const SettingsStorageId: symbol = Symbol();
 
 export interface SettingsStorageInterface {
-    load<R>(name: string, defaultValue: R): R;
+    load<R>(name: string, defaultValue: R): Promise<R>;
 
     save(name: string, value: any): void;
 }
